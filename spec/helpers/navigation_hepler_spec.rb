353afdfcb7f8ebd13c359dc7d46c074e6b3e3ddf
eichann <k.abe.in401@gmail.com>
2018-03-26 08:10:15 +0900

    fix syntax

--- spec/helpers/navigation_hepler_spec.rb
+++ spec/helpers/navigation_hepler_spec.rb
@@ -1,14 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe NavigationHelper, type: :helper do
-
   context 'signed in user' do
     before(:each) { allow(controller).to receive(:user_signed_in?).and_return(true) }
 
     context '#collapsible_links_partial_path' do
       it "returns signed_in_links partial's path" do
         expect(helper.collapsible_links_partial_path).to (
-          eq 'layouts/navigation/collapsible_elements/signed_in_links' )
+          eq 'layouts/navigation/collapsible_elements/signed_in_links')
       end
     end
   end
@@ -19,9 +18,8 @@ RSpec.describe NavigationHelper, type: :helper do
     context '#collapsible_links_partial_path' do
       it "returns non_signed_in_links partial's path" do
         expect(helper.collapsible_links_partial_path).to (
-          eq 'layouts/navigation/collapsible_elements/non_signed_in_links' )
+          eq 'layouts/navigation/collapsible_elements/non_signed_in_links')
       end
     end
   end
-
 end
